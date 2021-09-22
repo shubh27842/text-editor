1c554f3e37127ea4c3ed7b56bcc627a690ff9c2d
Shubham Ghule <sgshubhabhamghule7@gmail.com>
2021-09-22 16:15:26 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-ReactDOM.render( 
-  <Router>
-    <Switch>
-      <Route path="/">
-        <App />
-      </Route>
-    </Switch>
-  </Router>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />,document.getElementById('root'));
